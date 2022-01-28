062a1eebf7be7f1dee20775e1287ec6eddb8bff5
Kristina Uvarova <kduvarova@gmail.com>
2022-01-28 18:02:28 +0300

    28.01.22

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://img.freepik.com/free-vector/awesome-mascot-lion-logo_177315-8.jpg?size=338&ext=jpg&ga=GA1.2.1782550132.1640995200'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="http://www.catsmob.com/post/2012/06/01089/creative_facebook_timeline_covers_023.jpg"
+                 alt=""/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
