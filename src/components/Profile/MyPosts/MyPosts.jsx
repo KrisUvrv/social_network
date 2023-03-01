7ef429766983e65b40b2da2a3221b2ce008eee9d
KrisUvrv <kduvarova@gmail.com>
2023-03-01 15:17:13 +0100

    89

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -20,9 +20,11 @@ let AddNewPostForm = (props) => {
 
 let AddNewPostFormRedux = reduxForm({form: "ProfileAddNewPostForm"})(AddNewPostForm)
 
-const MyPosts = (props) => {
+const MyPosts = React.memo((props) => {
 
-    let postsElements = props.posts.map( p => < Post message={p.message} likesCount={p.likesCount}/>);
+    let postsElements = [...props.posts]
+        .reverse()
+        .map( p => < Post message={p.message} likesCount={p.likesCount}/>);
     let newPostElement = React.createRef();
 
     let onAddPost = (values) => {
@@ -38,6 +40,6 @@ const MyPosts = (props) => {
             </div>
         </div>
     )
-}
+});
 
 export default MyPosts;
